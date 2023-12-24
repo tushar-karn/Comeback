110099556a8761e417416cadf814ad80ea213a3e
tushar-karn <113161622+tushar-karn@users.noreply.github.com>
2023-12-24 16:37:57 +0530

    Array

---
+++ Array/intersectionOfArray.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int getIntrsection(vector<int>arr , vector<int>brr)
+{
+    vector<int>ans;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = 0; j < brr.size(); j++)
+        {
+            if (arr[i]==brr[j])
+            {
+                ans.push_back(arr[i]);
+            }
+        }
+    }
+    cout<<"INTERSECTION OF ARRAY IS ";
+    for(auto x: ans)
+    {
+        cout<<x<<" ";
+    }
+    
+}
+int main()
+{
+    int m;
+    cin>>m;
+    vector<int>arr(m);
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cin>>arr[i];
+    }
+
+    int n;
+    cin>>n;
+    vector<int>brr(n);
+    for (int i = 0; i < brr.size(); i++)
+    {
+        cin>>brr[i];
+    }
+
+    getIntrsection(arr,brr);
+    
+    
+    return 0;
+}
